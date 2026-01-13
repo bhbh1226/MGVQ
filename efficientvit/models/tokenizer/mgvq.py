0a8c2a116c620815605e1d96df19912008f17090
Pyeongjin Park <27873685+bhbh1226@users.noreply.github.com>
2026-01-13 09:46:15 +0900

    argparse를 가져와서 MGVQ 클래스의 load_model 메서드에서 안전한 글로벌 추가

--- efficientvit/models/tokenizer/mgvq.py
+++ efficientvit/models/tokenizer/mgvq.py
@@ -1,3 +1,5 @@
+import argparse
+
 from dataclasses import dataclass, field
 from typing import Any, Optional
 
@@ -419,6 +421,7 @@ class MGVQ(nn.Module):
 
     def load_model(self):
         if self.cfg.pretrained_source == "mgvq":
+            torch.serialization.add_safe_globals([argparse.Namespace])
             state_dict = torch.load(self.cfg.pretrained_path, map_location="cpu", weights_only=True)["state_dict"]
             self.load_state_dict(state_dict)
         else:
